ee4a2f1e8928670f105260ab150d2191d78ec8e9
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-07-30 20:19:46 +0200

    feat: add initial Leaflet map with geolocation and marker popup on click

--- script.js
+++ script.js
@@ -22,3 +22,42 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+// === MAP
+
+if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const { latitude } = position.coords;
+            const { longitude } = position.coords;
+            const coords = [latitude, longitude];
+
+            const map = L.map("map").setView(coords, 13);
+
+            L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            }).addTo(map);
+
+            map.on("click", function (mapEvent) {
+                console.log(mapEvent);
+                const { lat, lng } = mapEvent.latlng;
+
+                L.marker([lat, lng])
+                    .addTo(map)
+                    .bindPopup(
+                        L.popup({
+                            maxWidth: 250,
+                            minWidth: 50,
+                            autoClose: false,
+                            closeOnClick: false,
+                            className: "running-popup",
+                        }),
+                    )
+                    .setPopupContent("Workout")
+                    .openPopup();
+            });
+        },
+        function () {
+            alert("Cloud NOT get your position!");
+        },
+    );
